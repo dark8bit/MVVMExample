9a4f582faf09d78a41abf3ce9b00417be3e87949
Vladislav Klepikov <vkhelsov@mail.ru>
2022-03-15 18:22:17 +0400

    Add files via upload

---
+++ MVVMExample/Screens/Employees/ViewModel/EmployeeCellViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  EmployeeCellViewModel.swift
+//  MVVMExample
+//
+//  Created by Владислав Клепиков on 15.03.2022.
+//
+
+import Foundation
+
+struct EmployeeCellViewModel {
+    var id: String
+    var name: String
+    var salary: String
+    var age: String
+}
